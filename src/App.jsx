e5bbc3956ba655b0594cee2f585edf0096ddcbe9
Mohammad Edris <abdulkarimnezami@gmail.com>
2025-03-17 20:36:13 +0430

    make projress bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,18 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [progress,setProgress] = useState(0)
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <div className='App bg-slate-900 rounded-lg w-1/2 h-96 flex justify-center items-center flex-col m-auto' >
+    <div className="content w-full h-full mt-4 p-5">
+    <h1 className='text-4xl text-white' >Progress Bar</h1>
+     <progress id='myBar' className='w-full h-8 rounded-lg mt-20 bg-blue-600'  value={(progress <= 100 ) ? progress : 0 } max={100} >32%</progress>
+      
+    <br/>
+     <input type="text" className='border border-black w-1/2 m-5 p-2 rounded-md outline-none focus:ring-2 ring-blue-700' placeholder='Enter Progress Value' onChange={(e)=>setProgress(e.target.value)}/>
+     </div>
+   </div>
   )
 }
 
